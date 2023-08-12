2d774d1d8825a7d0b36ac17a012009d01a64d55f
omarc <omarcalderon1912@gmail.com>
2023-08-11 22:17:56 -0400

    compileOnly dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,12 +28,12 @@ kotlin {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
 
-    implementation("com.google.inject:guice:4.1.0")
-    implementation("com.google.inject.extensions:guice-multibindings:4.1.0")
-    implementation("com.authzee.kotlinguice4:kotlin-guice:1.3.0")
-    implementation("com.authzee.kotlinguice4:kotlin-guice-multibindings:1.3.0")
+    compileOnly("com.google.inject:guice:4.1.0")
+    compileOnly("com.google.inject.extensions:guice-multibindings:4.1.0")
+    compileOnly("com.authzee.kotlinguice4:kotlin-guice:1.3.0")
+    compileOnly("com.authzee.kotlinguice4:kotlin-guice-multibindings:1.3.0")
 
     implementation("com.github.DV8FromTheWorld:JDA:5.0.0-beta.5")
     implementation("net.dv8tion:JDA:5.0.0-beta.5") {
@@ -48,8 +48,7 @@ dependencies {
 
 tasks.shadowJar {
     relocate("org.slf4j", "dev.butter.mathbot.slf4j")
-    relocate("com.google.inject", "dev.butter.mathbot.guice")
-    relocate("com.authzee.kotlinguice4", "dev.authzee.kotlinguice4")
 
     archiveFileName.set("MathDiscordBot.jar")
+    destinationDirectory.set(File("C:/Users/omarc/Desktop/wine-test/plugins"))
 }
